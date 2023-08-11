7069637dff0c71a9ed02e1bffb4b8d73713e16cc
s1tf <119539022+s1tf@users.noreply.github.com>
2023-08-11 10:28:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pyxel
 
+import constants
+
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
+        pyxel.init(constants.WIDHT, constants.HEIGHT)
         self.x = 0
         pyxel.run(self.update, self.draw)
 
