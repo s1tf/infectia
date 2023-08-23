d1a25c94717ce34d15bd7560148ac0d4de202537
s1tf <119539022+s1tf@users.noreply.github.com>
2023-08-23 13:55:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,51 @@ import os
 import pyxel
 
 os.path.isfile('./constants.py')
-
 import constants
+from humans import Humans, Infected
 
 
 class App:
     def __init__(self):
+
+        # 1. Инициализируем приложение
         pyxel.init(constants.WIDTH, constants.HEIGHT)
-        self.x = 0
+
+        # 2. Создаём
+        self.humans = Humans()
+        self.infected = Infected()
+
+        # 3. Запускаем приложение
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        """ Обновляем """
+
+        # 1. Обновляем
+        self.humans.update()
+        self.infected.update()
+
+        # 2. Заражаем
+        for infected in self.infected.list:
+            for human in self.humans.list:
+                if infected.x == human.x and infected.y == human.y:
+                    human.color = pyxel.COLOR_RED
+                    self.infected.list.append(human)
+                    self.humans.list.remove(human)
+                    break
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.rect(self.x, 0, 16, 16, 1)
+        """ Рисуем """
+
+        # 1. Очищаем экран
+        pyxel.cls(constants.COLOR_BACKGROUND)
+
+        # 2. Рисуем
+        self.humans.draw()
+        self.infected.draw()
+
+        # 3. Рисуем количество заражённых
+        pyxel.text(1, 1, f'Infected: {len(self.infected.list)}', constants.COLOR_TEXT)
+
 
 App()
