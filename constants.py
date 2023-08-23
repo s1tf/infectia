adeb313b9e4c6bd3f46f36c6554dc5cccfa7c21b
s1tf <119539022+s1tf@users.noreply.github.com>
2023-08-23 13:54:53 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,14 @@
+import pyxel
+
 # Разрешение экрана
 WIDTH = 160
 HEIGHT = 90
+
+# Количество людей
+HUMANS_TOTAL = 500
+
+# Цвета
+COLOR_BACKGROUND = pyxel.COLOR_WHITE
+COLOR_TEXT = pyxel.COLOR_DARK_BLUE
+COLOR_HUMAN = pyxel.COLOR_BLACK
+COLOR_INFECTED = pyxel.COLOR_RED
